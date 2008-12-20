1954d5128d72c88a7cf7c2844dc58032d50d6cb7
admp <adomas@8magic.com>
2008-12-20 17:08:46 +0000

    Mailer: fix sender address and system URL.
    git-svn-id: http://svn.assembla.com/svn/make-it/timetrack@128 e0e46c49-be69-4f5a-ad62-21024a331aea

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -2,10 +2,10 @@ class Mailer < ActionMailer::Base
   def send_password(employee, password)
     subject    'Jums sukurtas vartotojas laiko apskaitos sistemoje'
     recipients employee.email
-    from       'time@projektai.lt'
+    from       'no-reply@makeit-time.com'
     sent_on    Time.now
     
     body       :login => employee.login, :password => password, 
-               :url => 'http://laikas.projektu-daktaras.lt/'
+               :url => 'http://www.makeit-time.com/'
   end
 end
